0859dc61f3769a429f34adb5ebc45c285598735c
UNKNOWN-MUSIC <130531308+KwsMusic@users.noreply.github.com>
2024-06-03 01:41:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,28 +4,28 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-### ❖ ➥
+### ❖ ➥ 𝗕𝐖𝗙™🇮🇳
 API_ID = getenv("API_ID", None)
 
-### ❖ ➥
+### ❖ ➥ 𝗕𝐖𝗙™🇮🇳
 API_HASH = getenv("API_HASH", None)
 
-### ❖ ➥
+### ❖ ➥ 𝗕𝐖𝗙™🇮🇳
 STRING_SESSION = getenv("STRING_SESSION", None)
 
-### ❖ ➥
-OWNER_ID = int(getenv("OWNER_ID", "6927241780"))
+### ❖ ➥ 𝗕𝐖𝗙™🇮🇳
+OWNER_ID = int(getenv("OWNER_ID", "5820831398"))
 
-### ❖ ➥
+### ❖ ➥ 𝗕𝐖𝗙™🇮🇳
 MONGO_URL = getenv("MONGO_URL", None)
 
-### ❖ ➥
-SUPPORT_GRP = getenv("SUPPORT_GRP", "Ravan_Lankaa")
+### ❖ ➥ 𝗕𝐖𝗙™🇮🇳
+SUPPORT_GRP = getenv("SUPPORT_GRP", "BWF_MUSIC1")
 
-### ❖ ➥
-UPDATE_CHNL = getenv("UPDATE_CHNL", "God_Ravana")
+### ❖ ➥ 𝗕𝐖𝗙™🇮🇳
+UPDATE_CHNL = getenv("UPDATE_CHNL", "BWF_MUSIC1")
 
-### ❖ ➥
-OWNER_USERNAME = getenv("OWNER_USERNAME", "GOD_R4V4N")
+### ❖ ➥ ༄𝐿 2 𝙍.🖤🜲𝐊𝐈𝐍𝐆❦︎ 𝆺𝅥⃝🍷
+OWNER_USERNAME = getenv("OWNER_USERNAME", "L2R_KING")
 
 
